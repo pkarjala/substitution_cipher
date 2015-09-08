b688bd08f28c5543cc78804d0c77810015f3cfcd
Patrick Karjala <pkarjala@gmail.com>
2015-09-07 18:05:08 -1000

    Clean up whitespace, add repeat for Monoalphabetic cipher.

--- cipher.py
+++ cipher.py
@@ -1,4 +1,5 @@
 # This is a test for various ciphers in Python.
+# Requires Python 3.x
 # It uses Z = 26, or the Roman Alphabet for character input.
 
 # Imports
@@ -11,69 +12,65 @@ rev_map = dict ( zip( char_map.values(), char_map.keys() ) )
 def get_key():
 	print( "\nPlease enter your key as a letter; i.e., a = b" )
 	key = input( "> " )
-	while len(key) > 1:
+	while len( key ) > 1:
 		print( "\nKey length is greater than one letter; please enter a single character key:")
 		key = input( "> " )
 	return char_map[key]
 
-def mono_cipher(message, enc_or_dec):
+def mono_cipher( message, enc_or_dec ):
 	cyphertext = ''
 	messagetext = ''
 	message = message.lower()
 	if enc_or_dec == 1:
 		for character in message:
 			encoded_text = char_map[character]
-			encrypted_text = (encoded_text + key) % 26
+			encrypted_text = ( encoded_text + key ) % 26
 			cyphertext += rev_map[encrypted_text]
 		return cyphertext.upper()
 
 	elif enc_or_dec == 2:
 		for character in message:
 			encoded_text = char_map[character]
-			decrypted_text = (encoded_text - key) % 26
+			decrypted_text = ( encoded_text - key ) % 26
 			messagetext += rev_map[decrypted_text]
 		return messagetext.lower()
 
-
 print( "\n\nPlease select a Substitution Cipher to use:" )
 
-cipher = int(input("Monoalphabetic [1]; Polyalphabetic [2]; Affine [3] \n> "))
+cipher_type = int( input( "Monoalphabetic [1]; Polyalphabetic [2]; Affine [3] \n> " ) )
 
-if cipher == 1:
+if cipher_type == 1:
 	print( "\nMonoalphabetic cipher chosen.\n" )
 	repeat = True
 	while repeat:
-		enc_or_dec = int(input("Encipher [1] or Decipher [2] \n> "))
+		enc_or_dec = int( input( "Encipher [1] or Decipher [2] \n> " ) )
 		key = get_key()
 		print( "\nKey is " + str(key) )
 		print( "\nPlease enter your message to be encrypted:" ) if enc_or_dec == 1 else print( "\nPlease enter your message to be decrypted:")
 		message = input("> ")
 		message = re.sub(r'\s+', '', message)
-		result = mono_cipher(message, enc_or_dec)
+		result = mono_cipher( message, enc_or_dec )
 		print( "\nEncrypted message is: " + result ) if enc_or_dec == 1 else print( "\nDecrypted message is: " + result )
 
-		print( "Do you wish to perform another Monoalphabetic cipher task?")
+		# To be broken out into separate management function
+		print( "\nDo you wish to perform another Monoalphabetic cipher task?" )
+		print( "[Y]es | [N]o" )
+		another = input( "> " )
+		while ( another not in ('y', 'Y', 'n', 'N') ):
+			print( "\nSorry, your input is not valid; do you wish to perform another cipher task?" )
+			print( "[Y]es | [N]o" )
+			another = input( "> " )
 
-		repeat = False
-		# elif enc_or_dec == 2:
-		# 	key = get_key()
-		# 	print( "\nKey is " + str(key) )
-		# 	print( "\nPlease enter your message to be decrypted:")
-		# 	message = input("> ")
-		# 	message = re.sub(r'\s+', '', message)
-		# 	message = message.lower()
-		# 	for character in message:
-		# 		enc_text = char_map[character]
-		# 		decrypted_text = enc_text - key % 26
-		# 		plaintext += rev_map[decrypted_text]
-		# 	print( "\nDecrypted message is: " + plaintext.upper() )
+		if another in ('n', 'N'):
+			repeat = False 
+		else :
+			repeat = True
 
-		# 	print( "Do you wish to perform another Monoalphabetic cipher task?")
-
-		# 	repeat = False
-elif cipher == 2:
-	print( "Polyalphabetic cipher chosen.")
-elif cipher == 3:
-	print( "Affine cipher chosen.")
+elif cipher_type == 2:
+	print( "\nPolyalphabetic cipher chosen.\n")
+	# To be added
+elif cipher_type == 3:
+	print( "\nAffine cipher chosen.\n")
+	# To be added
 else:
-	print( "Sorry; a valid choice was not made. Exiting...")
+	print( "\nSorry; a valid choice was not made.\n")
