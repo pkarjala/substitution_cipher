3744d9b1ddbdd6c174c51e1b459097fc95d940cc
Patrick Karjala <pkarjala@hawaii.edu>
2015-10-07 20:43:45 -1000

    Implement Extended Euclidian Algorithm.  Implement polyalphabetic Affine Cipher.  First pass on mono-alphabetic Affine Decipher.

--- ruby/cipher.rb
+++ ruby/cipher.rb
@@ -33,6 +33,7 @@ end
 # @param mapping [Map] The character to number hash table.
 #
 # @return [Array] The key array as integers.
+# NOTE:  Chokes on plain integer input; test.
 def translate_key(key, mapping)
   int_key = Array.new
   key.each_char do |character|
@@ -44,7 +45,7 @@ def translate_key(key, mapping)
 end
 
 
-# Performs shift cipher against any length key.
+# Performs Shift Cipher against any key length >= 1.
 #
 # @param key [String] the key, in plain text, of any length.
 # @param text [String] the plaintext, of any length, inclusive of incorrect characters.
@@ -63,7 +64,7 @@ def shift_cipher(key, text)
   return ciphertext
 end
 
-# Performs shift decipher against any length key.
+# Performs Shift Decipher against any key length >= 1.
 #
 # @param key [String] the key, in plain text, of any length.
 # @param text [String] the ciphertext, of any length.
@@ -71,15 +72,100 @@ end
 # @return [String] Returns the decrypted plaintext.
 def shift_decipher(key, text)
   plaintext = ''
-  encoded_text = ''
+  encrypted_text = ''
   key = translate_key(key, MY_DICT)
   ciphertext = clean_text(text, LETTERS)
   ciphertext.each_char.with_index do |character, index|
-    encoded_text = MY_DICT[character]
-    encrypted_text = (encoded_text - key[index % key.length]) % 26
-    plaintext += LETTERS[encrypted_text]
+    encrypted_text = MY_DICT[character]
+    encoded_text = (encrypted_text - key[index % key.length]) % 26
+    plaintext += LETTERS[encoded_text]
   end
   return plaintext
 end
+
+
+# Performs the extended euclidian algorithm to find the GCD of two numbers.
+# 
+# @param a [Integer] The first number.
+# @param b [Integer] The second number.
+#
+# @return [Array] The GCD of the two numbers, along with the final s & t values.
+def extended_euclidian_algorithm(a,b)
+  r0 = ''
+  r1 = ''
+  r0 = a
+  r1 = b
+  s0 = 1
+  s1 = 0
+  t0 = 0
+  t1 = 1
+  q = ''
+  temp = ''
+  while r1 != 0
+    q = (r0 / r1).floor
+    temp = r0
+    r0 = r1
+    r1 = temp - q * r1
+    temp = s0
+    s0 = s1
+    s1 = temp - q * s1
+    temp = t0
+    t0 = t1
+    t1 = temp - q * t1
+  end
+  d = r0
+  return [d, s0, t0]
+end
+
+
+# Performs an Affine Cipher against any two part key, where key length >=1.
+# 
+# Calculates A * m + B % 26
+#
+# @param key1 [String] The first part of the key A
+# @param key2 [String] The second part of the key B
+def affine_cipher(key1, key2, text)
+  ciphertext = ''
+  encoded_text = ''
+  a = translate_key(key1, MY_DICT)
+  b = translate_key(key2, MY_DICT)
+  message = clean_text(text, LETTERS)
+  message.each_char.with_index do |character, index|
+    encoded_text = MY_DICT[character]
+    encrypted_text = (encoded_text * a[index % a.length] + b[index % b.length]) % 26
+    ciphertext += LETTERS[encrypted_text]
+  end
+  return ciphertext
+end
+
+
+# Performs an Affine Cipher against any two part key, where key length >=1.
+# 
+# Calculates 1/A * (c - B) % 26
+#
+# @param key1 [String] The first part of the key A
+# @param key2 [String] The second part of the key B
+# NOTE:  Needs to be fixed to function with arrays for the a1 value
+def affine_decipher(key1, key2, text)
+  plaintext = ''
+  encrypted_text = ''
+  a1 = ''
+  a = translate_key(key1, MY_DICT)
+  b = translate_key(key2, MY_DICT)
+  a.each do |value|
+    gcd = extended_euclidian_algorithm(26, value)
+    a1 = gcd[2] % 26
+  end
+  ciphertext = clean_text(text, LETTERS)
+  ciphertext.each_char.with_index do |character, index|
+    encrypted_text = MY_DICT[character]
+    encoded_text = (encrypted_text * a1 - a1*b[index % b.length]) % 26
+    plaintext += LETTERS[encoded_text]
+  end
+  return plaintext
+end
+
+
+
 
 
--- ruby/testing.rb
+++ ruby/testing.rb
@@ -7,15 +7,27 @@ user_text = gets.chomp
 #user_text = clean_text(user_text, LETTERS)
 puts "Text is: #{user_text}"
 
-puts "Please provide a key: "
-user_key = gets.chomp
+puts "Please provide key 1: "
+user_key1 = gets.chomp
 #user_key = translate_key(user_key, MY_DICT)
-puts "Key is: #{user_key}"
+puts "Key is: #{user_key1}"
 
-ciphertext = shift_cipher(user_key, user_text)
+puts "Please provide key 2: "
+user_key2 = gets.chomp
+#user_key = translate_key(user_key, MY_DICT)
+puts "Key is: #{user_key2}"
 
-puts "The enciphered text is: " + ciphertext
+deciphertext = affine_decipher(user_key1, user_key2, user_text)
 
-deciphertext = shift_decipher(user_key, ciphertext)
+#ciphertext = shift_cipher(user_key1, user_text)
+
+# puts "The enciphered text is: " + ciphertext
 
 puts "The deciphered text is: " + deciphertext
+
+# deciphertext = shift_decipher(user_key, ciphertext)
+
+# puts "The deciphered text is: " + deciphertext
+
+# puts extended_euclidian_algorithm(26, 11)
+
